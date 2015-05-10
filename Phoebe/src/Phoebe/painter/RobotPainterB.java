413267679707f7cb0d62217123ad9b4014bcd07f
Bolgar Zsuzsanna <bol.zsuzs@gmail.com>
2015-05-10 20:21:26 +0200

    RobotPainterA,B osztály törölve

--- Phoebe/src/Phoebe/painter/RobotPainterB.java
+++
@@ -1,9 +0,0 @@
-package Phoebe.painter;
-
-/**
- * Created by K�vesdi on 2015.05.01..
- * NOT USED YET!!
- */
-public class RobotPainterB extends RobotPainter {
-    public RobotPainterB(String imageUrl) { super(imageUrl); }
-}
